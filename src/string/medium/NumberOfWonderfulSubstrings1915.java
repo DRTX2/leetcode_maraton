54609123af99389c8fea69b1e7405bab83b1d0fd
David Manjarres <davidmanjarres2004@gmail.com>
2025-08-03 16:57:43 -0500

    No entendi :C

---
+++ src/string/medium/NumberOfWonderfulSubstrings1915.java
@@ -0,0 +1,11 @@
+package string.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumberOfWonderfulSubstrings1915 {
+    public long wonderfulSubstrings(String word) {
+        
+        return 0;
+    }
+}
