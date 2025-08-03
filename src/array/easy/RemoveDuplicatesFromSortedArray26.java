530ca5fdf079b04cc4d66257d2cde21229322b63
David Manjarres <davidmanjarres2004@gmail.com>
2025-08-03 16:58:24 -0500

    Leetcode 26 - RemoveDuplicatesFromSortedArray

---
+++ src/array/easy/RemoveDuplicatesFromSortedArray26.java
@@ -0,0 +1,13 @@
+package array.easy;
+
+public class RemoveDuplicatesFromSortedArray26 {
+    public int removeDuplicates(int[] nums) {
+        int index =0;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[index]!=nums[i]){
+                nums[++index]=nums[i];
+            }
+        }
+        return ++index;
+    }
+}
