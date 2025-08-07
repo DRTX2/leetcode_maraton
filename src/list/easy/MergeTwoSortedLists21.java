e096d018b5b00b9f1f0e629fb0b457d06f1b5f2c
David Manjarres <davidmanjarres2004@gmail.com>
2025-08-06 19:28:05 -0500

    Leetcode 21 - MergeTwoSortedLists

---
+++ src/list/easy/MergeTwoSortedLists21.java
@@ -0,0 +1,21 @@
+package list.easy;
+
+import structures.ListNode;
+
+public class MergeTwoSortedLists21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode result = new ListNode(), current = result;
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                current.next = list1;
+                list1 = list1.next;
+            }else{
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current=current.next;
+        }
+        current.next=list1!=null?list1:list2;
+        return result.next;
+    }
+}
